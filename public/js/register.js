2cda33988346942b78c58a552ab2ef379c0f59e6
Darcio_ <bboydarcio@hotmail.com>
2024-07-20 23:33:44 -0300

    Modificação para db via json
    Modificação para db via json

--- public/js/register.js
+++ public/js/register.js
@@ -1,21 +1,22 @@
 document.getElementById('registerForm').addEventListener('submit', function(e) {
     e.preventDefault();
-
+  
     const username = document.getElementById('username').value;
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
-
+  
     fetch('/register', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ username, email, password })
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ username, email, password })
     })
     .then(response => response.text())
     .then(data => {
-        alert(data);
-        window.location.href = 'login.html';
+      alert(data);
+      window.location.href = 'login.html';
     })
     .catch(error => console.error('Error:', error));
-});
+  });
+  
